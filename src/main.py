fabce2f46f8cff5ce247f2b052705e84a3dfc09f
hjcas87 <castro.hernan5@gmail.com>
2026-01-13 23:35:39 +0000

    chore: docs added

--- src/main.py
+++ src/main.py
@@ -5,13 +5,15 @@ from src.helpers.extract_title import extract_title
 from src.helpers.create_content_site import create_content_site
 
 
-basepath = sys.argv
+
 dir_path_docs = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
 
 def main():
+    basepath = "/" if len(sys.argv) < 2 else sys.argv[1]
+    print(basepath)
     create_content_site(dir_path_docs)
     print("Generating page...")
     generate_pages_recursive(dir_path_content, template_path, dir_path_docs, basepath)
